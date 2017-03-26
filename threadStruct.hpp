55f7b5099485999006dd6cf61587048de416d8d6
Omer <Omertorrents@gmail.com>
2017-03-26 22:20:13 +0300

    we should implement more of the h functions

--- threadStruct.hpp
+++ threadStruct.hpp
@@ -2,64 +2,138 @@
 // Created by Omer on 24/03/2017.
 //
 
-#ifndef EX2_THREADSTRUCT_HPP
-#define EX2_THREADSTRUCT_HPP
-#define RUNNING 0
-#define WAITING 1
-#define BLOCKED 2
-#define ENDED 0
-#define PREEMPTED 1
-
-
 #include <stdio.h>
 #include <setjmp.h>
 #include <signal.h>
 #include <unistd.h>
 #include <sys/time.h>
+#include <array>
+
+#define SECOND 1000000
+#define STACK_SIZE 4096
+
+char stack1[STACK_SIZE];
+char stack2[STACK_SIZE];
+
+sigjmp_buf env[2];
+
+#ifdef __x86_64__
+/* code for 64 bit Intel arch */
+
+typedef unsigned long address_t;
+#define JB_SP 6
+#define JB_PC 7
+
+/* A translation is required when using an address of a variable.
+   Use this as a black box in your code. */
+address_t translate_address(address_t addr)
+{
+    address_t ret;
+    asm volatile("xor    %%fs:0x30,%0\n"
+            "rol    $0x11,%0\n"
+    : "=g" (ret)
+    : "0" (addr));
+    return ret;
+}
+
+#else
+/* code for 32 bit Intel arch */
+
+typedef unsigned int address_t;
+#define JB_SP 4
+#define JB_PC 5
+
+/* A translation is required when using an address of a variable.
+   Use this as a black box in your code. */
+address_t translate_address(address_t addr)
+{
+    address_t ret;
+    asm volatile("xor    %%gs:0x18,%0\n"
+		"rol    $0x9,%0\n"
+                 : "=g" (ret)
+                 : "0" (addr));
+    return ret;
+}
+
+
+#endif
+
+#define ARBITARY_VAL 1
+#define RUNNING 0
+#define WAITING 1
+#define BLOCKED 2
+#define ENDED 0
+#define PREEMPTED 1
 using namespace std;
+#define DEP_SIZE 100
+
+
+
 
 class spThread{
 public:
-    spThread(function func):foo(func){
-        foo = func;
+    /*
+     * creating new thread, the lower block of code was taken as is from demo_jmp.c
+     * on windows I C syntax error..
+     */
+    spThread(void (*f)(void), int tid):_relies_on(-1),_status(WAITING),_quant(0),_tid(tid){
+        address_t sp, pc;
+        _stack = new char[STACK_SIZE];
+        _needs_me = new int[DEP_SIZE];
+
+        sp = (address_t)_stack + STACK_SIZE - sizeof(address_t);
+        pc = (address_t)f;
+        sigsetjmp(_env, ARBITARY_VAL);
+        (_env->__jmpbuf)[JB_SP] = translate_address(sp);
+        (_env->__jmpbuf)[JB_PC] = translate_address(pc);
+        sigemptyset(&_env->__saved_mask);
     }
-    // running the thread
-    int run(){
-        signal(SIGVTALARM, handleTimeout);
-        if(env == NULL){
-            foo();
-        }
-        siglongjmp(env,currRun);
-        return ENDED;
+
+    /**
+     * small simple deleter
+     */
+    ~spThread(){
+        delete[](_stack);
+        delete[](_needs_me);
     }
-    // return status
-    void getStatus(){
-        return status;
+
+    /**
+     * after running function, saving the location of the function
+     */
+    int saveBuffer(){
+        return sigsetjmp(_env,ARBITARY_VAL);
+    }
+
+    /**
+     * increase the quants and running the thread again
+     */
+    void loadBuffer(){
+        _quant++;
+        siglongjmp(_env,ARBITARY_VAL);
+    }
+    /**
+     * getter for status
+     */
+    int getStatus(){
+        return _status;
     }
     // we can assume that there is no two dependency for specific thread
     // cause after we are calling sync we should stop the thread
     void setDep(int tid){
-        dependency = tid;
-        status = BLOCKED;
+        _relies_on = tid;
+        _status = BLOCKED;
     }
-    void freeDep(int tid){
-        if(status == BLOCKED && tid == dependency){
-            dependency = -1;
-            status = WAITING;
-        }
-    }
-    int handleTimeout(int sig_num){
-        ret_val = sigsetjmp(env,1);
-        return PREEMPTED;
+
+    void setStatus(int status){
+        _status = status;
     }
 
 private:
-    int status = WAITING;
-    function foo;
-    int dependency = -1;
-    sigjmp_buf env;
-    int currRun = 0;
+    int _status;
+    int _relies_on;
+    sigjmp_buf _env;
+    int _quant;
+    int* _needs_me;
+    char* _stack;
+    int _tid;
 };
-
-
-#endif //EX2_PROCESSSTRUCT_HPP
--- uthreads.cpp
+++ uthreads.cpp
@@ -1,8 +1,19 @@
 //
 // Created by Omer on 24/03/2017.
 //
+#include <map>
+#include <vector>
+#include <iostream>
 #include "uthreads.h"
 
+#define ERR_TITLE "uthreads.cpp : "
+
+#define INPUT_ERR 0
+
+#define SUCC 0
+#define FAIL -1
+
+#define QUANTUM_USEC "Quantum value should be bigger than 0"
 /*
  * Description: This function initializes the thread library.
  * You may assume that this function is called before any other thread library
@@ -11,4 +22,50 @@
  * function with non-positive quantum_usecs.
  * Return value: On success, return 0. On failure, return -1.
 */
-int uthread_init(int quantum_usecs);
+static map<int, spThread*> _threads;
+static vector<spThread*> _readyThreads;
+static vector<spThread*> _blockThreads;
+static spThread* _runningThread;
+static int quantom_overall = 0;
+struct itimerval _itTimer;
+struct sigaction _segActions;
+
+void error_log(int pCode, string tCode){
+    switch(pCode){
+        case INPUT_ERR:
+            cerr << ERR_TITLE << "Input error:" << tCode << endl;
+            break;
+        default:
+            break;
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+int uthread_init(int quantum_usecs){
+    // check if the given number is positive
+    if(quantum_usecs <= 0){
+        error_log(INPUT_ERR, QUANTUM_USEC);
+        return FAIL;
+    }
+//    we should define the calls handler here
+//    _segActions.sa_handler
+
+
+//  init!
+    // creating the main thread and adding it to the list
+    spThread* main = new spThread(nullptr, 0);
+    _threads.insert(pair<int, spThread*>(0, main));
+    _runningThread = main;
+    main->setStatus(RUNNING);
+    quantom_overall++;
+    return SUCC;
+}
